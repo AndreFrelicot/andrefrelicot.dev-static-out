a9ce88585b81269601d796086db8bc0003d4c8d9
André Frélicot <andre@frelicot.com>
2025-09-22 22:46:22 +0200

    fix 404

--- _next/static/chunks/a3e52e5e1a124f7d.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/5a6c3db863a354ce.js",
-  "static/chunks/f8194ed138d67973.js",
-  "static/chunks/turbopack-8bab919bc526fc51.js"
-])
---
+++ _next/static/chunks/e517a298ff4882de.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/6b297c6bc770653d.js",
+  "static/chunks/fcc9a7bfcf5377b1.js",
+  "static/chunks/turbopack-00dda0b80f9ab2e3.js"
+])
